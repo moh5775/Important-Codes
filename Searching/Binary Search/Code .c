a8b9a53a93ff200d0efc9de1602f971d9b123964
moh5775 <166235043+moh5775@users.noreply.github.com>
2024-09-07 13:27:05 +0600

    Create Code .c

---
+++ Searching/Binary Search/Code .c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+    int n,num;
+    scanf("%d %d",&n,&num);
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int temp;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    for(int i=0; i<n; i++)
+    {
+        printf("%d ",a[i]);
+    }
+    int k=-1;
+    int first=0;
+    int last=n-1;
+
+    while(first<=last)
+    {
+        int mid=(first+last)/2;
+        if(num>a[mid])
+        {
+            first=mid+1;
+        }
+        else if(num<a[mid])
+        {
+            last=mid-1;
+        }
+        else
+        {
+            k=mid;
+            break;
+        }
+    }
+    printf("\n index: %d\n",k);
+}
