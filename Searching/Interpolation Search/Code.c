e3bd77a3a84a5877ae66b1bb799146a6bef3b523
moh5775 <166235043+moh5775@users.noreply.github.com>
2024-09-16 16:10:42 +0600

    Update Searching

---
+++ Searching/Interpolation Search/Code.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    printf("Array size: ");
+    scanf("%d", &n);
+
+    int a[n];
+    printf("Enter Array Elements (sorted): ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    int key;
+    printf("Enter key: ");
+    scanf("%d", &key);
+
+    int index = -1;
+    int first = 0;
+    int last = n - 1;
+    if (a[first] == key) {
+        index = first;
+    }
+
+    else if(a[last]==key)
+    {
+        index = last;
+    }
+    else
+    {
+        while (first <= last) {
+
+            int mid = first + ((key - a[first]) * (last - first)) / (a[last] - a[first]);
+
+            if (a[mid] == key) {
+                index = mid;
+                break;
+            }
+            if (a[mid] < key) {
+                first = mid + 1;
+            } else {
+                last = mid - 1;
+            }
+        }
+
+
+    }
+    if (index == -1) {
+        printf("Not found\n");
+    }
+    else
+        printf("Index of the searching element: %d\n", index);
+
+    return 0;
+}
